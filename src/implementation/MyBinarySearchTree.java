8721d96bf371a72bbaee8d1bbf99a3082e95ffe8
TamannaAgnihotri <6391ta@gmail.com>
2021-04-13 23:20:23 +0530

    Insert,inOrder,preOrder,postOrder,reverseInOrder,search method implemented. Main method executed.

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,35 +5,100 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    private implementation.Node<E> root;
 
-    @Override
-    public void insert(E data) {
-
+    public implementation.Node<E> getRoot(){
+        return root;
     }
 
     @Override
-    public boolean search(E searchElement) {
+    public void insert(E data) {
+        implementation.Node<E> node = new implementation.Node<>(data);
+        if(isEmpty()){
+            root=node;
+        }
+        else{
+            implementation.Node<E> temp= (implementation.Node<E>) root;
+            implementation.Node<E> parent=null;
+            while(temp!=null){
+                parent=temp;
+                if(data.compareTo(temp.getData())<=0){
+                    temp=temp.getLeft();
+                }
+                else{
+                    temp=temp.getRight();
+                }
+            }
+
+            if(data.compareTo(parent.getData())<=0){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+        }
+    }
+
+    private boolean isEmpty(){
+        if(root==null){
+            return true;
+        }
         return false;
     }
 
     @Override
-    public void inOrder(Node<E> node) {
-
+    public boolean search(E searchElement) {
+        boolean response=false;
+        implementation.Node<E> temp = root;
+        while(temp!=null){
+            if(searchElement.compareTo(temp.getData())==0){
+                response=true;
+                break;
+            }
+            else if(searchElement.compareTo(temp.getData())<0){
+                temp=temp.getLeft();
+            }
+            else{
+                temp=temp.getRight();
+            }
+        }
+        return response;
     }
 
     @Override
-    public void preOrder(Node<E> node) {
-
+    public void inOrder(implementation.Node<E> node) {
+        if(node!=null){
+            inOrder(node.getLeft());
+            System.out.print(node.getData()+",");
+            inOrder(node.getRight());
+        }
     }
 
     @Override
-    public void postOrder(Node<E> node) {
-
+    public void preOrder(implementation.Node<E> node) {
+        if(node!=null){
+            System.out.print(node.getData()+",");
+            preOrder(node.getLeft());
+            preOrder(node.getRight());
+        }
     }
 
     @Override
-    public void reverseInOrder(Node<E> node) {
+    public void postOrder(implementation.Node<E> node) {
+        if(node!=null){
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            System.out.print(node.getData()+",");
+        }
+    }
 
+    @Override
+    public void reverseInOrder(implementation.Node<E> node) {
+        if(node!=null){
+            reverseInOrder(node.getRight());
+            System.out.print(node.getData()+",");
+            reverseInOrder(node.getLeft());
+        }
     }
 
     @Override
@@ -42,7 +107,10 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
     }
 
     @Override
-    public int height(Node<E> node) {
+    public int height(implementation.Node<E> node) {
         return 0;
     }
+
+
+
 }
--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,19 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
-        //create binary search tree
+        MyBinarySearchTree<Integer> tree = new MyBinarySearchTree<>();
+        tree.insert(45);
+        tree.insert(30);
+        tree.insert(55);
+        tree.insert(60);
+        tree.insert(20);
+        System.out.println("Data Inserted");
+
+        System.out.println("Presence of Element : "+tree.search(55));
+        System.out.println("Presence of Element : "+tree.search(25));
         // and perform insert, search, traversal, delete
     }
 }
